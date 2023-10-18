b748b097484dc3743b621e15794584d1138cac09
GabrielA <gabrielalvesmatosdearaujo77@gmail.com>
2023-10-18 11:50:53 -0300

    All complet project

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Saiu novo filme",
+    "description": "Olha o novo filme que saiu hoje!",
+    "photoCover": "https://lh3.googleusercontent.com/OTGdG8vte_p0tFLS8XQ1mEHidWr8KhrBpf-BSUGwK4Fss9-kcuSZnO3BvMZKiaf4XebF=s170"
+  },
+  {
+    "id": "2",
+    "title": "Novo filme do homem de ferro",
+    "description": "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium mollitia tempore facilis repudiandae laboriosam dolor dicta, sed debitis culpa, molestias enim. Saepe accusamus harum sapiente sit perferendis, doloremque eveniet quas?",
+    "photoCover": "https://i.pinimg.com/736x/b8/cf/f4/b8cff40efdda049aab503a0115fef514.jpg"
+  }
+]
